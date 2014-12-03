dac8eba5b9814a2f29deaada43a7c7b1982aee5b
Dave Paola <dpaola2@gmail.com>
2014-12-02 17:43:32 -0800

    morphed? and morphed_from helpers

--- app/helpers/morphing/application_helper.rb
+++ app/helpers/morphing/application_helper.rb
@@ -1,4 +1,11 @@
 module Morphing
   module ApplicationHelper
+    def morphed_from
+      @morphed_from ||= User.find(session[:morphed_from])
+    end
+
+    def morphed?
+      session[:morphed_from].present?
+    end
   end
 end
