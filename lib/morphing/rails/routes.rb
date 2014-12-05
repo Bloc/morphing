0992c3961b0952019e055eb044589b453f779c55
Dave Paola <dpaola2@gmail.com>
2014-12-04 16:22:41 -0800

    proper string interpolation

--- lib/morphing/rails/routes.rb
+++ lib/morphing/rails/routes.rb
@@ -1,8 +1,10 @@
 module ActionDispatch::Routing
   class Mapper
-    def morphing_for
-      get '/morph/:id'    => 'morphing/morphing#morph', as: :morph
-      get '/unmorph'      => 'morphing/morphing#unmorph', as: :unmorph
+    def morphing_for(options = {})
+      controller_name = options[:controller] || 'morphing/morphing'
+
+      get '/morph/:id'    => "#{controller_name}#morph", as: :morph
+      get '/unmorph'      => "#{controller_name}#unmorph", as: :unmorph
     end
   end
 end
