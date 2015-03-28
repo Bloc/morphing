46bab5d6be5896e90e15914201483e0cb2e7fb57
Jared Rader & Christian Schlensker <raderj89+pair+raderj89.wordofchristian@gmail.com>
2015-03-28 01:06:56 -0700

    allow morphing many levels deep

--- app/helpers/morphing/application_helper.rb
+++ app/helpers/morphing/application_helper.rb
@@ -1,10 +1,11 @@
 module Morphing
   module ApplicationHelper
     def morphed_from
-      @morphed_from ||= User.find(session[:morphed_from])
+      @morphed_from ||= User.find(session[:morphed_from].last)
     end
 
     def morphed?
+      # binding.pry
       session[:morphed_from].present?
     end
   end
